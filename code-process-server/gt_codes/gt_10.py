2a67d09cb4b462fff9d4754b84664871cea7c8f0
박정양 <80745010+wjddid000624@users.noreply.github.com>
2026-08-03 15:15:14 +0900

    new_version_260803_5

--- code-process-server/gt_codes/gt_10.py
+++ code-process-server/gt_codes/gt_10.py
@@ -1,3 +1,5 @@
+MOD = 1000000007
+
 def padovan_optimized(n):
     if n == 1 or n == 2 or n == 3:
         return 1
@@ -5,7 +7,7 @@ def padovan_optimized(n):
     a = b = c = 1
 
     for i in range(n-3):
-        a, b, c = b, c, a + b
+        a, b, c = b, c, (a + b) % MOD
 
     return c
  
